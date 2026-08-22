893d5245a6d238ffb2c99ca175cc752a9ddb7749
yamariel <ariel.yamien@epitech.eu>
2026-08-22 17:08:14 +0000

    [UPDATE]: construction de l'application

--- lib/main.dart
+++ lib/main.dart
@@ -1,122 +1,207 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_grid/responsive_grid.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MonApplication());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MonApplication extends StatelessWidget {
+  const MonApplication({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: const BottomNavigationBarPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class MapremierePage extends StatefulWidget {
+  const MapremierePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MapremierePage> createState() => _MapremierePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MapremierePageState extends State<MapremierePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        leading: const Icon(Icons.search),
+        backgroundColor: Colors.amber,
+        actions: [Icon(Icons.list)],
+        title: Text(
+          "Vos émissions en streaming",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: PartieGrilleImage(),
+    );
+  }
+}
 
-  void _incrementCounter() {
+class PartieGrilleImage  extends StatelessWidget{
+  const PartieGrilleImage ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ResponsiveGridList(
+       desiredItemWidth: 100.0,
+       minSpacing: 5,
+       children: [
+        IdentificationStreaming(
+            tagStream: "Streaming News", 
+            imageStream: "assets/images/news.jpg", 
+            nomStream: "Que des news", 
+            chaineRadio: "Radio 1"
+          ),
+    
+        IdentificationStreaming(
+            tagStream: "Streaming News", 
+            imageStream: "assets/images/news.jpg", 
+            nomStream: "Que des news 2", 
+            chaineRadio: "Radio 2"
+          ),
+        IdentificationStreaming(
+            tagStream: "Streaming News", 
+            imageStream: "assets/images/news.jpg", 
+            nomStream: "Que des news 3", 
+            chaineRadio: "Radio 3"
+          ),
+    
+        IdentificationStreaming(
+            tagStream: "Streaming News", 
+            imageStream: "assets/images/news.jpg", 
+            nomStream: "Que des news 4", 
+            chaineRadio: "Radio 4"
+          ),
+    
+        IdentificationStreaming(
+            tagStream: "Streaming News", 
+            imageStream: "assets/images/news.jpg", 
+            nomStream: "Que des news 5", 
+            chaineRadio: "Radio 5"
+          ),
+    
+        IdentificationStreaming(
+            tagStream: "Streaming News", 
+            imageStream: "assets/images/news.jpg", 
+            nomStream: "Que des news 6", 
+            chaineRadio: "Radio 6"
+          ),
+       ],
+    );
+  }
+}
+
+class BottomNavigationBarPage extends StatefulWidget {
+  const BottomNavigationBarPage({super.key});
+
+  @override
+  State<BottomNavigationBarPage> createState() => _BottomNavigationBarPageState();
+}
+
+class _BottomNavigationBarPageState extends State<BottomNavigationBarPage> {
+  int _currentIndex = 0;
+  final List<Widget> _pages = [
+    const MapremierePage(),
+    const Center(child: Text("Recherche")),
+    const Center(child: Text("Profil")),
+  ];
+
+  void _setWidget(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _currentIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: .center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: _setWidget,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Accueil"),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Recherche"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profil"),
+        ],
       ),
     );
+  }
+}
+
+//Model
+class IdentificationStreaming extends StatelessWidget {
+  final String tagStream;
+  final String imageStream;
+  final String nomStream;
+  final String chaineRadio;
+  const IdentificationStreaming({
+    super.key,
+    required this.tagStream,
+    required this.imageStream,
+    required this.nomStream,
+    required this.chaineRadio,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [BoxShadow(color: Colors.black.withValues(alpha: 0.3))],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: GestureDetector(
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => AlbumStreaming(chaineRadio: chaineRadio, nomStream: nomStream, imageStream: imageStream)),
+            );
+          },
+          child: Column(
+            children: [
+              Hero(tag: tagStream, 
+              child: Image.network(
+                  imageStream,
+                  width: double.infinity,
+                  height: 180,
+                  fit: BoxFit.cover,
+                )
+              ),
+              Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(12),
+                child: Column(
+                  children: [
+                    Text(nomStream),
+                    const SizedBox(height: 5,),
+                    Text(chaineRadio)
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AlbumStreaming extends StatelessWidget {
+  final String imageStream;
+  final String nomStream;
+  final String chaineRadio;
+
+  const AlbumStreaming({super.key, required this.chaineRadio, required this.nomStream, required this.imageStream});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:streaming/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const MonApplication());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
