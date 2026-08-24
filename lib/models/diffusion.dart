bd3d7d95e53ada82362828d917f16e480ba1338d
yamariel <ariel.yamien@epitech.eu>
2026-08-24 21:43:51 +0000

    [ADD]: ajout des models de données

---
+++ lib/models/diffusion.dart
@@ -0,0 +1,11 @@
+class Diffusion {
+  final String nom;
+  final DateTime date;
+  final int duree;
+
+  Diffusion({
+    required this.nom,
+    required this.date,
+    required this.duree,
+  });
+}
---
+++ lib/models/emission.dart
@@ -0,0 +1,13 @@
+class Emission {
+  final int id;
+  final String nom;
+  final String chaine;
+  final String image;
+
+  Emission({
+    required this.id,
+    required this.nom,
+    required this.chaine,
+    required this.image,
+  });
+}
